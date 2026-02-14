f8ee5eaff6a9fddc7ce23bf1b19dca02a4d217b6
hdweb2025 <hd.website19@gmail.com>
2026-02-14 13:13:37 +0700

    fix: Update catch-all route to '/*'

--- server/api-server.js
+++ server/api-server.js
@@ -67,8 +67,8 @@ const distPath = path.join(__dirname, '..', 'dist');
 app.use(express.static(distPath));
 
 // Rute catch-all untuk menyajikan index.html (untuk React Router)
-app.get('*', (req, res) => {
-    res.sendFile(path.join(distPath, 'index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
 });
 
 
