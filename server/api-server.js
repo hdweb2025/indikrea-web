f9a4f2522e0de1e07fd1cf79736a9c3e47e0cbff
hdweb2025 <hd.website19@gmail.com>
2026-02-14 13:16:09 +0700

    fix: Use regex for catch-all route to avoid path-to-regexp issue

--- server/api-server.js
+++ server/api-server.js
@@ -67,7 +67,7 @@ const distPath = path.join(__dirname, '..', 'dist');
 app.use(express.static(distPath));
 
 // Rute catch-all untuk menyajikan index.html (untuk React Router)
-app.get('/*', (req, res) => {
+app.get(/^\/(?!api).*/, (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
