2244e1ac6ab4887cc146de3277a04b55bdd1361f
Husen Nh <hd.website19@gmail.com>
2026-02-16 14:35:52 +0700

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -34,7 +34,7 @@ const Login: React.FC<LoginProps> = ({ userType }) => {
         setIsLoading(true);
 
         try {
-            const response = await fetch(`${API_BASE_URL}/update`, {
+            const response = await fetch(`${API_BASE_URL}/data`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
